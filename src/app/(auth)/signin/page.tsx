d0174ea3ef41a73b0363d67517a4d7fa0c336a48
Aman Thukral <thukral618@gmail.com>
2024-06-30 15:41:34 +0530

    Adding logic for signin on frontend

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,20 +1,80 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+const SignIn = () => {
+  const [userName, setUserName] = useState("");
+  const [userNameMessage, setUserNameMessage] = useState("");
+  const [isCheckingUserName, setIsCheckingUserName] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const debouncedUserName = useDebounceValue(userName, 300);
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button>Sign in</button>
-    </>
-  );
-}
+  //zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      userName: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUserNameUniue = async () => {
+      if (debouncedUserName) {
+        setIsCheckingUserName(true);
+        setUserNameMessage("");
+        try {
+          const resposne = await axios.get(
+            `/api/check-username-unique?userName=${debouncedUserName}`
+          );
+          setUserNameMessage(resposne.data.message);
+        } catch (error) {
+          const AxiosError = error as AxiosError<ApiResponse>;
+          setUserNameMessage(
+            AxiosError.response?.data.message || "Error checking userName"
+          );
+        } finally {
+          setIsCheckingUserName(false);
+        }
+      }
+    };
+    checkUserNameUniue();
+  }, [debouncedUserName]);
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>("/api/signup", data);
+      toast({
+        title: "Sucess",
+        description: response.data.message,
+      });
+      router.replace(`/verify/${userName}`);
+      setIsSubmitting(false);
+    } catch (error) {
+      console.error("Error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Signup Failed",
+        description: errorMessage,
+        variant: "destructive",
+      });
+      setIsSubmitting(false);
+    }
+  };
+  return <div>page</div>;
+};
+
+export default SignIn;
