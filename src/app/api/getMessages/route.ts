108f89eb908929c6b76129e589dc42cb5af6456b
Aman Thukral <thukral618@gmail.com>
2024-08-23 17:18:10 +0530

    Add Dashboard component

--- src/app/api/getMessages/route.ts
+++ src/app/api/getMessages/route.ts
@@ -8,9 +8,9 @@ import mongoose from "mongoose";
 export async function GET(request: Request) {
   await dbConnect();
   const session = await getServerSession(authOptions);
-  const user: User = session?.user as User;
+  const _user: User = session?.user as User;
 
-  if (!session || !session.user) {
+  if (!session || !_user) {
     return Response.json(
       {
         success: false,
@@ -20,15 +20,15 @@ export async function GET(request: Request) {
     );
   }
 
-  const userId = new mongoose.Types.ObjectId(user._id);
+  const userId = _user._id;
 
   try {
     const user = await UserModel.aggregate([
-      { $match: { id: userId } },
-      { $unwind: "messages" },
+      { $match: { _id: userId } },
+      { $unwind: "$messages" },
       { $sort: { "messages.createdAt": -1 } },
       { $group: { _id: "$_id", messages: { $push: "$messages" } } },
-    ]);
+    ]).exec();
 
     if (!user || user.length === 0) {
       return Response.json(
